9a5cd7ab042ff5db4b38c59e65bae015a3de47ca
Nathan Osman <nathan.osman@crans.org>
2014-04-30 14:18:50 -0400

    Corrected some bugs

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ PORT = 12800
 
 connection_with_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection_with_server.connect((HOST, PORT))
-print("Connexion établie avec le serveur sur le port {}".format(port))
+print("Connexion établie avec le serveur sur le port {}".format(PORT))
 
 data = b""
 while data != b"fin":
@@ -18,6 +18,6 @@ while data != b"fin":
     connection_with_server.send(data)
     answer = connection_with_server.recv(1024)
     print(answer.decode()) # Là encore, peut planter s'il y a des accents
-
+    
 print("Fermeture de la connexion")
 connection_with_server.close()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*-coding:Latin-1 -*
 
-import os
 import socket
 import select
 
@@ -15,27 +14,27 @@ main_connection.listen(5)
 server_running = True
 connected_clients = []
 while server_running:
-	connections_pending, wlist, xlist = select.select([main_connection], [], [], 0.05)
-	for connection in connections_pending:
-		connection_with_client, connections_infos = connection.accept()
-		connected_client.append(connection_with_client)
-	
-	clients_waiting = []
-	try:
-		clients_waiting, wlist, xlist = select.select(connected_clients, [], [], 0.05)
-	except select.error:
-		pass
-	else:
-		for client in clients_waiting:
-			data = client.recv(1024)
-			print(data.decode())
-			client.send(b"OK")
-			if data == END_SIGNAL:
-				server_running = False
+        connections_pending, wlist, xlist = select.select([main_connection], [], [], 0.05)
+        for connection in connections_pending:
+                connection_with_client, connections_infos = connection.accept()
+                connected_clients.append(connection_with_client)
+
+        clients_waiting = []
+
+        try:
+                clients_waiting, wlist, xlist = select.select(connected_clients, [], [], 0.05)
+        except select.error:
+                pass
+        else:
+                for client in clients_waiting:
+                        data = client.recv(1024).decode()
+                        print(data)
+                        client.send(b"OK")
+                        if data == END_SIGNAL:
+                                server_running = False
 
 for client in connected_clients:
-	client.close()
+        client.send(b"END_SIGNAL received, connection closing.")
+        client.close()
 
 main_connection.close()
-
-os.system("pause")
